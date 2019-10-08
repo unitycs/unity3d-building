851cb1e2e465b386f23021e46bc1897bbf884fb3
eamon <eamon_chen@outlook.com>
2019-10-08 22:29:15 +0800

    update project scripts

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class fz : MonoBehaviour
+public class Building : MonoBehaviour
 {
     public GameObject gm1;
     public GameObject gm2;
