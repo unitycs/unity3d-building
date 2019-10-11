9273c397cf8b9d2d2b79484a581695e4c02ff3df
eamon <eamon_chen@outlook.com>
2019-10-12 00:18:31 +0800

    add scripts

---
+++ Assets/Scripts/CameraFollowTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowTarget : MonoBehaviour
+{
+
+    public Transform Robot;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - Robot.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Robot.position + offset;
+    }
+}
---
+++ Assets/Scripts/RobotAutoCtrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class RobotAutoCtrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //    RaycastHit hit;
+            if(Physics.Raycast(ray,out RaycastHit hit)){
+
+                agent.SetDestination(hit.point);
+
+            }
+        }
+    }
+}
