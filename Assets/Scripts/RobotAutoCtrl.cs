8bca9d87f6bb3e9eed9fbd120bcd325a5505c941
eamon <eamon_chen@outlook.com>
2019-10-16 08:43:09 +0800

    add robot scripts

--- Assets/Scripts/RobotAutoCtrl.cs
+++ Assets/Scripts/RobotAutoCtrl.cs
@@ -2,23 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-public class RobotAutoCtrl : MonoBehaviour
-{
-    // Start is called before the first frame update
+
+[RequireComponent (typeof (Animator))]
+public class RobotAutoCtrl : MonoBehaviour {
 
     public NavMeshAgent agent;
+    public Transform FollowedCamera;
+    protected Animator m_Anim;
+    CharacterController m_Ctrl;
+    public float m_Speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start () {
+        m_Anim = GetComponent<Animator> ();
+        m_Ctrl = GetComponent<CharacterController> ();
+        //  HeartMax = 4;
+        //  HeartNow = 3;
+    }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0)){
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    void Update () {
 
-        //    RaycastHit hit;
-            if(Physics.Raycast(ray,out RaycastHit hit)){
+        var h = Input.GetAxis ("Horizontal");
+        var v = Input.GetAxis ("Vertical");
 
-                agent.SetDestination(hit.point);
+        if (h != 0 || v != 0) {
+            var mDir = new Vector3 (v * FollowedCamera.transform.forward.x + h * FollowedCamera.transform.right.x,
+                0, v * FollowedCamera.transform.forward.z + h * FollowedCamera.transform.right.z);
+            Quaternion newRotation = Quaternion.LookRotation (mDir);
+            transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, 0.9f);
 
+            m_Ctrl.Move (mDir * Time.deltaTime);
+            if (!m_Anim.GetBool ("Run")) m_Anim.SetBool ("Run", true);
+        } else {
+            if (m_Anim.GetBool ("Run")) m_Anim.SetBool ("Run", false);
+        }
+
+        if (Input.GetMouseButtonDown (0)) {
+            var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+            //    RaycastHit hit;
+            if (Physics.Raycast (ray, out RaycastHit hit)) {
+
+                agent.SetDestination (hit.point);
             }
         }
     }
